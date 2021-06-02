3424948754838bb1691171e4ad075ceb793797f2
wangyu <wangyu@yeelight.com>
2021-06-02 15:30:36 +0800

    feat([null safe]): support null safe

--- lib/wifi_ip.dart
+++ lib/wifi_ip.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class WifiIpInfo {
-  final String ip;
-  final String netmask;
-  final String broadcastIP;
+  final String? ip;
+  final String? netmask;
+  final String? broadcastIP;
 
   WifiIpInfo.fromMap(Map<String, String> map)
       : ip = map["ip"],
@@ -21,6 +21,6 @@ class WifiIp {
   static Future<WifiIpInfo> get getWifiIp async {
     final Map wifiInfoMap =
         Map<String, String>.from(await _channel.invokeMethod('getWifiIp'));
-    return WifiIpInfo.fromMap(wifiInfoMap);
+    return WifiIpInfo.fromMap(wifiInfoMap as Map<String, String>);
   }
 }
